480cc4212faad2dfaabe30b88a9056fde1853f12
Aleksandr Turchenko <alexandrt100@gmail.com>
2023-08-19 14:52:38 +0200

    Add ButtonInfo component

---
+++ src/components/ButtonInfo/index.tsx
@@ -0,0 +1,32 @@
+import React, { useRef, useState } from "react";
+import useClickOutsideEvent from "../../hooks/useOutside";
+
+const ButtonInfo: React.FC = () => {
+  const [showInfo, setShowInfo] = useState(false);
+  const container = useRef<HTMLDivElement>(null);
+  useClickOutsideEvent(container, () => setShowInfo(false));
+
+  const handleInfoClick = () => setShowInfo(!showInfo);
+
+  return (
+    <>
+      <button className="w-8 h-8" onClick={() => handleInfoClick()}>
+        <img src="information.svg" alt="info" className="w-8 h-8" />
+      </button>
+      {showInfo && (
+        <div
+          ref={container}
+          className="absolute top-12 right-0 w-64 p-4 text-sm text-left bg-white border border-gray-300 rounded-md shadow-lg"
+        >
+          <p className="p-1">
+            Ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec diam
+            eget risus ultricies feugiat. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi.
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ButtonInfo;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "--color-ap-fields-enabled": "#E0E0E0",
+        "--color-ap-bg-white": "#fff",
+        "--color-brand-bluetiful-bluetiful-95": "#EDF2FC",
+        "--color-ap-link-enabled": "#4d7ae3",
+      },
+      screens: {
+        xs: "320px",
+        md: "768px",
+        lg: "1024px",
+      },
+    },
   },
   plugins: [],
 };
